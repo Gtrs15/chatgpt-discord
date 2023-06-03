20dc0fa37875815675474dad591e53abe9360a2f
Brooks F <gtrsurge+ubs@gmail.com>
2023-06-03 15:18:59 -0700

    update: added str split function

--- utils/str_manipulators.py
+++ utils/str_manipulators.py
@@ -5,3 +5,22 @@ def extract_guild_ids(text):
     pattern = r'Guild id=(\w+)'
     matches = re.findall(pattern, text)
     return matches
+
+
+# Splits string so that it does not reach discord char limit
+def split_string(message, max_chars):
+    words = message.split()
+    result = []
+    current = ''
+    for word in words:
+        if len(current) + len(word) + 1 <= max_chars:
+            # add word to current piece
+            current += ' ' + word if current else word
+        else:
+            # add current piece to result list and start new piece
+            result.append(current)
+            current = word
+    # add last piece to result list
+    if current:
+        result.append(current)
+    return result
