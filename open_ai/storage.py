c1a375835a5b48a31cb35790008d9c3d3ed99d0e
Brooks F <gtrsurge+ubs@gmail.com>
2023-06-03 09:33:16 -0700

    Init

---
+++ open_ai/storage.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import shelve
+
+
+class ChatStorageBox:
+
+    def __init__(self, database_folder_path='data', database_file_path='data.db'):
+        pass
+
+    def verify_database_folder_path(self, database_folder_path):
+        pass
+
+    def verify_database_file(self, database_folder_path, database_file_path):
+        pass
+
+    def write_value_to_db_list(self, value):
+        pass
+
+    def read(self):
+        pass
+
+    def clear(self):
+        pass
